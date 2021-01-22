c186a4cf13ae7ac8e4ca1761b6c77b9b260f5e1d
ia_pon4ik <bozhidarnikviktorr@gmail.com>
2021-01-22 17:58:22 +0200

    t

---
+++ js.js
@@ -0,0 +1,44 @@
+var controls = document.querySelectorAll('.controls');
+for(var i=0; i<controls.length; i++){
+  controls[i].style.display = 'inline-block';
+}
+
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,5000);
+
+function nextSlide(){
+  goToSlide(currentSlide+1);
+}
+
+function previousSlide(){
+  goToSlide(currentSlide-1);
+}
+
+function goToSlide(n){
+  slides[currentSlide].className = 'slide';
+  currentSlide = (n+slides.length)%slides.length;
+  slides[currentSlide].className = 'slide showing';
+}
+
+
+var playing = true;
+var pauseButton = document.getElementById('pause');
+
+
+
+var next = document.getElementById('next');
+var previous = document.getElementById('previous');
+
+
+next.onclick = function(){
+  pauseSlideshow();
+  nextSlide();
+
+};
+previous.onclick = function(){
+  pauseSlideshow();
+  previousSlide();
+};
+
+
